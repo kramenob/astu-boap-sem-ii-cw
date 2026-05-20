099ccb4f912f6f772d8d8a6eef71b67cc1b46f85
Mark <ruisdaeless@yahoo.com>
2026-05-20 09:58:29 +0400

    Initial Commit

---
+++ boap-sem-ii-cw/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  boap-sem-ii-cw
+//
+//  Created by Mark on 20/05/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
