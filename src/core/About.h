15c94f0d9fd0322b895229f2966eb0e81dd73709
Mark <ruisdaeless@yahoo.com>
2026-06-09 23:50:04 +0400

    grated

--- src/core/About.h
+++ src/core/About.h
@@ -13,10 +13,10 @@
  *
  * Compile-time constants describing the application.
  */
-#define VERSION     "0.1.0"
+#define VERSION     "1.0.0"
 #define NAME        "markforge"
 #define DESCRIPTION "A versatile CLI tool for managing databases, generating templates, and more."
-#define COPYRIGHT   R"(Copyright (C) 2026 Maxim Bolotin.
+#define COPYRIGHT   R"(Copyright (C) 2026 Mark Bone.
 Repository: https://github.com/kramenob/markforge
 This is free software; see the source for copying conditions. There is no
 warranty, not even for merchantability or fitness for a particular purpose.)"
